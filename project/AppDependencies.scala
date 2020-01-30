44411082ae09da919dc53042abaaa589c0595036
jgrath <jgrath@users.noreply.github.com>
2020-01-30 10:18:25 +0000

    DL-2667 - PLA SBT Upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,36 +1,7 @@
-/*
- * Copyright 2016 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
-object MicroServiceBuild extends Build with MicroService {
+object AppDependencies {
 
-  val appName = "pensions-lifetime-allowance"
-
-  override lazy val plugins: Seq[Plugins] = Seq(
-    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
-  )
-
-  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
-}
-
-private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
@@ -88,4 +59,3 @@ private object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ Test() ++ IntegrationTest()
 }
-
